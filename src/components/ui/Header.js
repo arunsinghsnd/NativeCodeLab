1dd6388d4dd01b746ba5b5eafcf996d63b7d27c1
arunsinghsnd <arunsinghsnd@gmail.com>
2021-11-01 19:36:01 +0530

    styles the header tab

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -3,6 +3,8 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import { makeStyles } from "@material-ui/core/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
 import logo from "../../assets/logo.svg";
 
@@ -26,6 +28,14 @@ const useStyles = makeStyles(theme => ({
   logo: {
     height: "7em",
   },
+  tabContainer: {
+    marginLeft: "auto",
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: "25px",
+  },
 }));
 
 const Header = () => {
@@ -36,6 +46,13 @@ const Header = () => {
         <AppBar position="fixed" color="primary">
           <Toolbar disableGutters>
             <img className={classes.logo} src={logo} alt="company logo" />
+            <Tabs className={classes.tabContainer}>
+              <Tab className={classes.tab} label="Home" />
+              <Tab className={classes.tab} label="Services" />
+              <Tab className={classes.tab} label="The Revolution" />
+              <Tab className={classes.tab} label="About Us" />
+              <Tab className={classes.tab} label="Contact Us" />
+            </Tabs>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
