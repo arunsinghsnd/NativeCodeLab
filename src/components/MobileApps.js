c2c5d37e709cab4d3aec3e415494b66684dcff41
arunsinghsnd <arunsinghsnd@gmail.com>
2021-12-28 13:29:45 +0530

    added the integration and platform support sections

--- src/components/MobileApps.js
+++ src/components/MobileApps.js
@@ -14,6 +14,8 @@ import backArrow from "../assets/backArrow.svg";
 import forwardArrow from "../assets/forwardArrow.svg";
 import Lottie from "react-lottie";
 
+import integrationAnimation from "../animations/integrationAnimation/data.json";
+
 const useStyles = makeStyles(theme => ({
   heading: {
     maxWidth: "40em",
@@ -40,6 +42,15 @@ const MobileApps = props => {
   const matchesMD = useMediaQuery(theme.breakpoints.down("md"));
   const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
   const matchesXS = useMediaQuery(theme.breakpoints.down("xs"));
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: false,
+    animationData: integrationAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
     <Grid container direction="column">
       <Grid
@@ -120,6 +131,46 @@ const MobileApps = props => {
             </IconButton>
           </Grid>
         </Hidden>
+      </Grid>
+      <Grid item container direction="row" className={classes.rowContainer}>
+        <Grid item container direction="column" md>
+          <Grid item>
+            <Typography variant="h4" gutterBottom>
+              Integration
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="body2" paragraph>
+              Our teachnology enables an innate interconnection between web and
+              mobile applications, putting everything you need right in one
+              convenient place.
+            </Typography>
+            <Typography variant="body2" paragraph>
+              This allows you to extends your reach, reivent interactions, and
+              develop a stronger relationship with your users than ever before.
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item md>
+          <Lottie options={defaultOptions}></Lottie>
+        </Grid>
+        <Grid item container direction="column" md>
+          <Grid item>
+            <Typography align="right" variant="h4" gutterBottom>
+              Simultaneous Platform Support
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography align="right" variant="body2" paragraph>
+              Our cutting-edge developemnt process allows us to create apps for
+              iPhone, Android, and tables - all the same tinme.
+            </Typography>
+            <Typography align="right" variant="body2" paragraph>
+              This significantly reduces costs and creates a more unified brand
+              experience across all devices.
+            </Typography>
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
