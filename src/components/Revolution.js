9e3e56b79c629b38fc010c4faa0a5c2fdbbbf1cd
arunsinghsnd <arunsinghsnd@gmail.com>
2021-12-28 20:54:13 +0530

    initial setup is compeleted to the rfevoluatiomn page

---
+++ src/components/Revolution.js
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  Grid,
+  makeStyles,
+  useTheme,
+  Typography,
+  useMediaQuery,
+  IconButton,
+  Hidden,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+import CallToAction from "./ui/CallToAction";
+
+const useStyles = makeStyles(theme => ({
+  heading: {
+    maxWidth: "40em",
+  },
+  arrowContainer: {
+    marginTop: "0.5em",
+  },
+  rowContainer: {
+    paddingLeft: "5em",
+    paddingRight: "5em",
+    [theme.breakpoints.down("sm")]: {
+      paddingLeft: "1.5em",
+      paddingRight: "1.5em",
+    },
+  },
+  paragraphContainer: {
+    maxWidth: "30em",
+  },
+}));
+
+const Revolution = props => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const matchesMD = useMediaQuery(theme.breakpoints.down("md"));
+  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
+  const matchesXS = useMediaQuery(theme.breakpoints.down("xs"));
+  return (
+    <Grid container direction="column">
+      <Grid item>
+        <Typography variant="h2" style={{ fontFamily: "Pacifico" }}>
+          The Revolution
+        </Typography>
+      </Grid>
+      <Grid item>
+        <CallToAction setValue={props.setValue} />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Revolution;
