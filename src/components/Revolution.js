5911e91c15585d5687c5f964be2262f66fd07512
arunsinghsnd <arunsinghsnd@gmail.com>
2021-12-29 12:45:46 +0530

    created the vision section to the Revolution page

--- src/components/Revolution.js
+++ src/components/Revolution.js
@@ -10,6 +10,8 @@ import {
 } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
+import vision from "../assets/vision.svg";
+
 import CallToAction from "./ui/CallToAction";
 
 const useStyles = makeStyles(theme => ({
@@ -40,11 +42,66 @@ const Revolution = props => {
   const matchesXS = useMediaQuery(theme.breakpoints.down("xs"));
   return (
     <Grid container direction="column">
-      <Grid item>
+      <Grid item className={classes.rowContainer} style={{ marginTop: "2em" }}>
         <Typography variant="h2" style={{ fontFamily: "Pacifico" }}>
           The Revolution
         </Typography>
       </Grid>
+      <Grid
+        item
+        container
+        direction="row"
+        alignItems="center"
+        className={classes.rowContainer}
+      >
+        <Grid item lg>
+          <img
+            src={vision}
+            alt="mountain through binoculrs"
+            style={{ maxWidth: "40em", marginRight: "5em" }}
+          />
+        </Grid>
+
+        <Grid item container direction="column" lg style={{ maxWidth: "40em" }}>
+          <Grid item>
+            <Typography variant="h4" align="right" gutterBottom>
+              Vision
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="body2" align="right" paragraph>
+              The rise of compouters , and subsequently the Internet , has
+              completely altered every aspect of humans life. This has increased
+              our comfort, broadened our connections, and reshaped how we view
+              the world.
+            </Typography>
+            <Typography variant="body2" align="right" paragraph>
+              What once was confined to huge rooms and teams of engineers now
+              reside in every single one of our hands. Harnessing this unlimited
+              potenial by using it to solve problems and better lives is at the
+              heart of everything we do.
+            </Typography>
+            <Typography variant="body2" align="right" paragraph>
+              We want businesses capitalize on the latest and greatest
+              technology. The best way to predict the future is to be the one
+              building it, and we want to help guide the world into this next
+              chapter of technological expansion, and innovation.
+            </Typography>
+            <Typography variant="body2" align="right" paragraph>
+              By holding ourselves to rigorous standards and pristine quatlity,
+              we can ensure you have the absolute best tools necessary to thrive
+              in this new frontier.
+            </Typography>
+            <Typography variant="body2" align="right" paragraph>
+              We see a future where every individual has personalized software
+              custom tailored to their lifestyle, culture, and interests,
+              helping them overcome life's obstacles. Each project is a step
+              towards that goal.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+
       <Grid item>
         <CallToAction setValue={props.setValue} />
       </Grid>
