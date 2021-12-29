47edf9d3ca367e114a144c6d4ae73a5b73ebc4c3
arunsinghsnd <arunsinghsnd@gmail.com>
2021-12-29 18:35:49 +0530

    added the history section of about page

--- src/components/About.js
+++ src/components/About.js
@@ -77,6 +77,28 @@ const About = () => {
             >
               We're the new kid on the block
             </Typography>
+            <Typography variant="body2" paragraph>
+              Founded in 2019, we're ready to get our hands on the world's
+              business problems.
+            </Typography>
+            <Typography variant="body2" paragraph>
+              It all started with one question: Why aren't all businesses using
+              available technology? There are many different answers to that
+              question: economic barriers, social barriers,
+            </Typography>
+            <Typography variant="body2" paragraph>
+              We aim to be a powerful force in overcoming these obstacles.
+              Recent developments in software engineering and computing power,
+              compounded by the proliferation of smart phones, has opened up
+              infinite worlds of possibility. Things that have always been done
+              by hand can now be done digitally and automatically, and
+              completely new methods of interaction are created daily. Taking
+              full advantage of these advancements is the name of the game.
+            </Typography>
+            <Typography variant="body2" paragraph>
+              All this change can be a lot to keep up with, and that's where we
+              come in.
+            </Typography>
           </Grid>
         </Grid>
       </Grid>
