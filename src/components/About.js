d7d07c995c2ff5617a22362d88cc21cd2249b361
arunsinghsnd <arunsinghsnd@gmail.com>
2021-12-29 18:51:57 +0530

    fixed the alignemnt of history section

--- src/components/About.js
+++ src/components/About.js
@@ -8,6 +8,7 @@ import {
   IconButton,
   Hidden,
 } from "@material-ui/core";
+import history from "../assets/history.svg";
 
 const useStyles = makeStyles(theme => ({
   missionStatement: {
@@ -62,43 +63,65 @@ const About = () => {
           to the Midwest and beyond.
         </Typography>
       </Grid>
-      <Grid item container className={classes.rowContainer}>
-        <Grid item container direction="column">
-          <Grid item>
-            <Typography variant="h4" gutterBottom>
-              Histtory
-            </Typography>
+      <Grid
+        item
+        container
+        className={classes.rowContainer}
+        justifyContent="space-around"
+      >
+        <Grid item>
+          <Grid
+            item
+            container
+            direction="column"
+            lg
+            style={{ maxWidth: "35em" }}
+          >
+            <Grid item>
+              <Typography variant="h4" gutterBottom>
+                History
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography
+                variant="body2"
+                paragraph
+                style={{ fontWeight: 700, fontStyle: "italic" }}
+              >
+                We're the new kid on the block
+              </Typography>
+              <Typography variant="body2" paragraph>
+                Founded in 2019, we're ready to get our hands on the world's
+                business problems.
+              </Typography>
+              <Typography variant="body2" paragraph>
+                It all started with one question: Why aren't all businesses
+                using available technology? There are many different answers to
+                that question: economic barriers, social barriers,
+              </Typography>
+              <Typography variant="body2" paragraph>
+                We aim to be a powerful force in overcoming these obstacles.
+                Recent developments in software engineering and computing power,
+                compounded by the proliferation of smart phones, has opened up
+                infinite worlds of possibility. Things that have always been
+                done by hand can now be done digitally and automatically, and
+                completely new methods of interaction are created daily. Taking
+                full advantage of these advancements is the name of the game.
+              </Typography>
+              <Typography variant="body2" paragraph>
+                All this change can be a lot to keep up with, and that's where
+                we come in.
+              </Typography>
+            </Grid>
           </Grid>
-          <Grid item>
-            <Typography
-              variant="body2"
-              paragraph
-              style={{ fontWeight: 700, fontStyle: "italic" }}
-            >
-              We're the new kid on the block
-            </Typography>
-            <Typography variant="body2" paragraph>
-              Founded in 2019, we're ready to get our hands on the world's
-              business problems.
-            </Typography>
-            <Typography variant="body2" paragraph>
-              It all started with one question: Why aren't all businesses using
-              available technology? There are many different answers to that
-              question: economic barriers, social barriers,
-            </Typography>
-            <Typography variant="body2" paragraph>
-              We aim to be a powerful force in overcoming these obstacles.
-              Recent developments in software engineering and computing power,
-              compounded by the proliferation of smart phones, has opened up
-              infinite worlds of possibility. Things that have always been done
-              by hand can now be done digitally and automatically, and
-              completely new methods of interaction are created daily. Taking
-              full advantage of these advancements is the name of the game.
-            </Typography>
-            <Typography variant="body2" paragraph>
-              All this change can be a lot to keep up with, and that's where we
-              come in.
-            </Typography>
+        </Grid>
+        <Grid item>
+          <Grid item container justifyContent="center" lg>
+            <img
+              src={history}
+              alt="quill pen sitting on top of book"
+              style={{ maxHeight: "22em" }}
+            />
           </Grid>
         </Grid>
       </Grid>
