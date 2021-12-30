d4bde9eabe04109fe55a0631ca439313a248e0c4
arunsinghsnd <arunsinghsnd@gmail.com>
2021-12-30 07:51:47 +0530

    added the phone and email section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,6 +11,8 @@ import {
   TextField,
 } from "@material-ui/core";
 import background from "../assets/background.jpg";
+import phoneIcon from "../assets/phone.svg";
+import emailIcon from "../assets/email.svg";
 
 const useStyles = makeStyles(theme => ({
   background: {
@@ -26,9 +28,10 @@ const Contact = props => {
   const classes = useStyles();
   const theme = useTheme();
   const matchesMD = useMediaQuery(theme.breakpoints.down("md"));
+
   return (
     <Grid container direction="row">
-      <Grid item container direction="column" lg={3}>
+      <Grid item container direction="column" justifyContent="center" lg={3}>
         <Grid item>
           <Typography variant="h2" style={{ lineHeight: 1 }}>
             Contcat Us
@@ -39,6 +42,43 @@ const Contact = props => {
           >
             We're waiting.
           </Typography>
+        </Grid>
+        <Grid item container>
+          <Grid item>
+            <img
+              src={phoneIcon}
+              alt="phone icon"
+              style={{ marginRight: "0.5em" }}
+            />
+          </Grid>
+          <Grid item>
+            {/* <img src={emailIcon} alt="phone icon" /> */}
+            <Typography
+              variant="body2"
+              style={{ color: theme.palette.common.blue, fontSize: "1rem" }}
+            >
+              (+91) 8427825355
+            </Typography>
+          </Grid>
+        </Grid>
+
+        <Grid item container>
+          <Grid item>
+            <img
+              src={emailIcon}
+              alt="envalop"
+              style={{ marginRight: "0.5em", verticalAlign: "bottom" }}
+            />
+          </Grid>
+          <Grid item>
+            {/* <img src={emailIcon} alt="phone icon" /> */}
+            <Typography
+              variant="body2"
+              style={{ color: theme.palette.common.blue, fontSize: "1rem" }}
+            >
+              arunsinghsnd@gmail.com
+            </Typography>
+          </Grid>
         </Grid>
       </Grid>
 
