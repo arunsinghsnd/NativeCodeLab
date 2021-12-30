156413feab828560efe4153f7be03adb679efb40
arunsinghsnd <arunsinghsnd@gmail.com>
2021-12-30 09:36:50 +0530

    added the phone and mail validation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -82,9 +82,47 @@ const Contact = props => {
   const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
 
   const [name, setName] = useState("");
+
   const [email, setEmail] = useState("");
+  const [emailHelper, setEmailHelper] = useState("");
+
   const [phone, setPhone] = useState("");
+  const [phoneHelper, setPhoneHelper] = useState("");
+
   const [message, setMessage] = useState("");
+
+  const onChange = event => {
+    let valid;
+
+    switch (event.target.id) {
+      case "email":
+        setEmail(event.target.value);
+        valid = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(
+          event.target.value
+        );
+
+        if (!valid) {
+          setEmailHelper("Invaild email");
+        } else {
+          setEmailHelper("");
+        }
+        break;
+      case "phone":
+        setPhone(event.target.value);
+        valid = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(
+          event.target.value
+        );
+
+        if (!valid) {
+          setPhoneHelper("Invaild phone");
+        } else {
+          setPhoneHelper("");
+        }
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <Grid container direction="row">
@@ -174,19 +212,23 @@ const Contact = props => {
               <Grid item style={{ marginBottom: "0.5em" }}>
                 <TextField
                   label="Email"
+                  error={emailHelper.length !== 0}
+                  helperText={emailHelper}
                   id="email"
                   fullWidth
                   value={email}
-                  onChange={event => setEmail(event.target.value)}
+                  onChange={onChange}
                 />
               </Grid>
               <Grid item>
                 <TextField
                   label="Phone"
+                  error={phoneHelper.length !== 0}
+                  helperText={phoneHelper}
                   id="phone"
                   fullWidth
                   value={phone}
-                  onChange={event => setPhone(event.target.value)}
+                  onChange={onChange}
                 />
               </Grid>
             </Grid>
